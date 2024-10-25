9972c8212eb62585b750322647be886c5468c38e
Macflylegamer <110659988+Macflylegamer@users.noreply.github.com>
2024-10-25 14:12:38 +0200

    Update MyAddonFunction.lua

--- MyAddonFunction.lua
+++ MyAddonFunction.lua
@@ -15,7 +15,7 @@ end
 print(ClearItemsGroupBox)
 
 ClearItemsGroupBox:AddToggle('MyToggle', {
-    Text = 'Update 6.75!',
+    Text = 'Update 7!',
     Default = true, -- Default value (true / false)
     Tooltip = 'This is a tooltip', -- Information shown when you hover over the toggle
 
@@ -32,3 +32,22 @@ for _, outerFrame in ipairs(ClearItemsGroupBox.Container:GetChildren()) do
         break
     end
 end
+
+-- Multi dropdowns
+ClearItemsGroupBox:AddDropdown('MyMultiDropdown', {
+    -- Default is the numeric index (e.g. "This" would be 1 since it if first in the values list)
+    -- Default also accepts a string as well
+
+    -- Currently you can not set multiple values with a dropdown
+
+    Values = { 'This', 'is', 'a', 'dropdown' },
+    Default = 1,
+    Multi = true, -- true / false, allows multiple choices to be selected
+
+    Text = 'A dropdown',
+    Tooltip = 'This is a tooltip', -- Information shown when you hover over the dropdown
+
+    Callback = function(Value)
+        print('[cb] Multi dropdown got changed:', Value)
+    end
+})
