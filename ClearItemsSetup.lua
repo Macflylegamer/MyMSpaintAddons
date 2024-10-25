7e4011bc520ee6c74fb008afac04d6707cf0d534
Macflylegamer <110659988+Macflylegamer@users.noreply.github.com>
2024-10-25 20:37:32 +0200

    Update ClearItemsSetup.lua

--- ClearItemsSetup.lua
+++ ClearItemsSetup.lua
@@ -1,5 +1,5 @@
--- Accepting the isAutoSetup argument
-local function setupAddon(isAutoSetup)
+-- Accepting the isManualSetup argument 
+local function setupAddon(isManualSetup)
     local Tabs = {
         -- Creates a new tab titled Main
         AddonTab = shared.Window.Tabs["Addons [BETA]"],
@@ -17,7 +17,7 @@ local function setupAddon(isAutoSetup)
     print(ClearItemsGroupBox)
 
     ClearItemsGroupBox:AddToggle('MyToggle', {
-        Text = 'Update 7!',
+        Text = 'Update 9!',
         Default = true, -- Default value (true / false)
         Tooltip = 'This is a tooltip', -- Information shown when you hover over the toggle
 
@@ -27,7 +27,7 @@ local function setupAddon(isAutoSetup)
     })
 
     -- Delete Button only if isAutoSetup is true
-    if isAutoSetup then
+    if isManualSetup then
         for _, outerFrame in ipairs(ClearItemsGroupBox.Container:GetChildren()) do
             local label = outerFrame:FindFirstChildOfClass('Frame') and outerFrame:FindFirstChildOfClass('Frame'):FindFirstChildWhichIsA('TextLabel')
             if label and label.Text == "Setup Clear Items" then
@@ -40,11 +40,6 @@ local function setupAddon(isAutoSetup)
 
     -- Multi dropdowns
     ClearItemsGroupBox:AddDropdown('MyMultiDropdown', {
-        -- Default is the numeric index (e.g. "This" would be 1 since it is first in the values list)
-        -- Default also accepts a string as well
-
-        -- Currently, you cannot set multiple values with a dropdown
-
         Values = { 'This', 'is', 'a', 'dropdown' },
         Default = 1,
         Multi = true, -- true / false, allows multiple choices to be selected
@@ -58,5 +53,5 @@ local function setupAddon(isAutoSetup)
     })
 end
 
--- Call the setupAddon function, passing the value from the loadstring
+-- Return the function so it can be called later
 return setupAddon
