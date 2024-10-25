fbd89f922b24127ea9f01d83f63b37f900ca66dc
Macflylegamer <110659988+Macflylegamer@users.noreply.github.com>
2024-10-25 07:20:13 +0200

    Update MyAddonFunction.lua

--- MyAddonFunction.lua
+++ MyAddonFunction.lua
@@ -16,7 +16,7 @@ end
 print(ClearItemsGroupBox)
 
 ClearItemsGroupBox:AddToggle('MyToggle', {
-    Text = 'Update 4.5!',
+    Text = 'Update 5!',
     Default = true, -- Default value (true / false)
     Tooltip = 'This is a tooltip', -- Information shown when you hover over the toggle
 
@@ -29,10 +29,13 @@ local container = ClearItemsGroupBox.Container
 for _, element in ipairs(container:GetChildren()) do
     print("element : " .. element.name);
     if element:IsA('Frame') or element:IsA('TextLabel') then
+        if element:IsA('TextLabel') then print(element.Text) end
         for _, element1 in ipairs(element:GetChildren()) do
             print("element1 : " .. element1.name);
-            if element:IsA('Frame') or element:IsA('TextLabel') then
+            if element1:IsA('Frame') or element1:IsA('TextLabel') then
+                if element1:IsA('TextLabel') then print(element1.Text) end
                 for _, element2 in ipairs(element1:GetChildren()) do
+                    if element2:IsA('TextLabel') then print(element2.Text) end
                     print("element2 : " .. element2.name);
                 end
             end
