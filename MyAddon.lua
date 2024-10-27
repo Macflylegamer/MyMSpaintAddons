68633014707ab0c0d6dfa4b44d0ff1ce65668c72
Macflylegamer <110659988+Macflylegamer@users.noreply.github.com>
2024-10-27 02:14:52 +0200

    Update MyAddon.lua

--- MyAddon.lua
+++ MyAddon.lua
@@ -2,8 +2,8 @@ local Options = getgenv().Linoria.Options
 local Toggles = getgenv().Linoria.Toggles
 
 local Addon = {
-    Name = "ClearItemsManualSetup",
-    Title = "Clear Items Manual Setup",
+    Name = "RemoveItemsManualSetup",
+    Title = "Remove Items Manual Setup",
     Description = "The manual setup of my addon (cuz dropdown doesn't work without this)",
 
     Game = {
@@ -16,12 +16,12 @@ local Addon = {
             Type = "Button",
             Name = "SetupButton",
             Arguments = {
-                Text = 'Setup Clear Items',
-                Tooltip = 'Setup the Clear Items addon (because dropdown is broken using the normal thing)',
+                Text = 'Setup Remove Items',
+                Tooltip = 'Setup the Remove Items addon (because dropdown is broken using the normal thing)',
 
                 Func = function()
                     -- Load the script and call setupAddon with true
-                    local setup = loadstring(game:HttpGet("https://raw.githubusercontent.com/Macflylegamer/MyMSpaintAddons/refs/heads/main/ClearItemsSetup.lua"))()
+                    local setup = loadstring(game:HttpGet("https://raw.githubusercontent.com/Macflylegamer/MyMSpaintAddons/refs/heads/main/RemoveItemsSetup.lua"))()
                     if setup then
                         setup(true)  -- Call the setupAddon function with true
                     else
